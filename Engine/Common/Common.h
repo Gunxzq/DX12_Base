72128951cba70674fa3a4a1941749d24d6d43cff
Gunxzq <3219988985@qq.com>
2026-05-25 23:33:12 +0800

    更全面的公共头文件

--- Engine/Common/Common.h
+++ Engine/Common/Common.h
@@ -10,4 +10,12 @@
 // ---------- 平台目标版本 ----------
 #include <SDKDDKVer.h>
 
+// ---------- ImGui 包装 ----------
 #include "ImGuiWrapper.h"
+
+// ---------- DirectX 包装 ----------
+#include "d3dUtil.h"
+
+// ---------- 其他 ----------
+#include "MathHelper.h"
+#include "ThrowHelper.h"
